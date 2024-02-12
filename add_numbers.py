caf59559dcfb148f5f9ac9ec44ecc7ba02b404cd
Lelcaren <lelcaren11@gmail.com>
2024-02-12 22:05:09 +0300

    my python practice

--- add_numbers.py
+++ add_numbers.py
@@ -27,3 +27,115 @@ Output:
     Output:
 
         Sum of 15 and 12 is 27
+          
+          Add Two Numbers in Python Using Function
+          This program show adding two numbers in python using function.         We can define a function that accepts two integers and returns         their sum.
+
+                                                                            #To define a function that take two integers
+                                                                            #and return the sum of those two numbers
+                                                                            def add(a,b):
+                                                                                return a+b
+                                                                            #initializing the variables
+
+    num1 = 10
+    num2 = 5
+    
+    #function calling and store the result into sum_of_twonumbers
+    sum_of_twonumbers = add(num1,num2)
+    
+    #To print the result
+    print("Sum of {0} and {1} is {2};" .format(num1,num2, sum_of_twonumbers))
+
+    Output 
+    15
+    
+
+        Using operator.add() method
+
+        # Python program to add two numbers
+        
+        num1 = 15
+        num2 = 12
+
+        # Adding two nos
+        import operator
+        sum = operator.add(num1,num2)
+
+        #printing values
+        print("sum of {0} and {1} is {2}" .format(num1,num2,sum))
+        
+        Output
+        27
+
+        using lambda function
+        #Define a lambda function to add two numbers
+        add_numbers = labda x, y: x+y
+        # Take input from the user
+        num1 = 1
+        num2 = 2
+        # Call thelambda function to add the two numbers
+        result = add_numbers(num1, num2)
+        #print the result
+        print("The sum of",num1,"and",num2, "is", result)
+        
+        Output:
+            The sum of 1 and 2 is 3
+
+
+            iAdd Two7
+              Add Two Numbers 
+              Add Two Numbers in Python Using Function
+              This program show adding two numbers in python using function.         We can define a function that accepts two integers and returns         their sum.
+
+                                                                                #To define a function that take two integers
+                                                                                #and return the sum of those two numbers
+                                                                                def add(a,b):
+                                                                                    return a+b
+                                                                                #initializing the variables
+
+        num1 = 10
+        num2 = 5
+        
+        #function calling and store the result into sum_of_twonumbers
+        sum_of_twonumbers = add(num1,num2)
+        
+        #To print the result
+        print("Sum of {0} and {1} is {2};" .format(num1,num2, sum_of_twonumbers))
+
+        Output 
+        15
+        U
+
+            Using operator.add() method
+
+            # Python program to add two numbers
+            
+            num1 = 15
+            num2 = 12
+
+            # Adding two nos
+            import operator
+            sum = operator.add(num1,num2)
+
+            #printing values
+            print("sum of {0} and {1} is {2}" .format(num1,num2,sum))
+            
+            Output
+            27
+
+            using lambda function
+            #Define a lambda function to add two numbers
+            add_numbers = labda x, y: x+y
+            # Take input from the user
+            num1 = 1
+            num2 = 2
+            # Call thelambda function to add the two numbers
+            result = add_numbers(num1, num2)
+            #print the result
+            print("The sum of",num1,"and",num2, "is", result)
+            
+            Output:
+                The sum of 1 and 2 is 3
+
+
+
---
+++ maximum_of_2_numbers.py
@@ -0,0 +1,89 @@
+An approach where we will compare two numbers using if-else
+statement and will print the output accordingly.
+
+# python program to find
+# maximum of two numbers
+
+    def maximum(a, b):
+        if a >= b:
+            return a
+        else:
+            return b
+
+    # Driver code
+    a = 2
+    b = 4
+    print(maximum(a,b))
+    Output
+    4
+
+    Using max().function
+     This function is used to find the maximum of the values passed as      its arguments.
+
+     # Python program to find the 
+     #maximum of two numbers
+
+     a = 2
+     b = 4
+
+     maximum = max(a, b)
+     print(maximum)
+     Output
+     4
+
+     Conditional expression
+      # Python program to find the
+      # maximum of two numbers
+        
+        # Driver code
+        a = 2
+        b = 4
+
+        # Use of ternary operator
+        print(a if a >= b else b)
+             Output
+             4An approach where we will compare two numbers using if-else
+             statement and will print the output accordingly.
+
+             # python program to find
+             # maximum of two numbers
+
+                def maximum(a, b):
+                    if a >= b:
+                        return a
+                    else:
+                        return b
+
+                # Driver code
+                a = 2
+                b = 4
+                print(maximum(a,b))
+                Output
+                4
+
+                Using max().function
+                 This function is used to find the maximum of the values passed as      its arguments.
+
+                 # Python program to find the 
+                 #maximum of two numbers
+
+                 a = 2
+                 b = 4
+
+                 maximum = max(a, b)
+                 print(maximum)
+                 Output
+                 4
+
+                 Conditional expression
+                  # Python pro# Python program to find the
+                  # maximum of two numbers
+                    
+                    # Driver code
+                    a = 2
+                    b = 4
+
+                    # Use of ternary operator
+                    print(a if a >= b else b)
+                         Output
+                         4
