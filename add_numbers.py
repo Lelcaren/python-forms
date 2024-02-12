02189a172d638d778d0c79aa025b776ce634e692
Lelcaren <lelcaren11@gmail.com>
2024-02-12 13:52:41 +0300

    my python practice

---
+++ add_numbers.py
@@ -0,0 +1,29 @@
+Add Two Numbers with “+” Operator
+
+# Python3 program to add two numbers
+num1 = 15
+num2 = 12
+
+# Adding two nos
+sum = num1 + num2
+
+# printing values
+print("Sum of", num1, "and", num2 , "is", sum)
+
+Output:
+
+    Sum of 15 and 12 is 27Add Two Numbers with “+” Operator
+
+    # Python3 program to add two numbers
+    num1 = 15
+    num2 = 12
+
+    # Adding two nos
+    sum = num1 + num2
+
+    # printing values
+    print("Sum of", num1, "and", num2 , "is", sum)
+
+    Output:
+
+        Sum of 15 and 12 is 27
